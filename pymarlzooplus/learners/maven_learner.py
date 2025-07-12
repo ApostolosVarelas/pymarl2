0116e4cb74c9e6bfdeaebfa049b5c51b1071c699
Apostolos Varelas <87095756+ApostolosVarelas@users.noreply.github.com>
2025-07-12 18:44:46 +0300

    Update maven_learner.py

--- pymarlzooplus/learners/maven_learner.py
+++ pymarlzooplus/learners/maven_learner.py
@@ -171,7 +171,7 @@ class MavenLearner:
 
         if t_env - self.log_stats_t >= self.args.learner_log_interval:
             self.logger.log_stat("loss", loss.item(), t_env)
-            self.logger.log_stat("grad_norm", grad_norm, t_env)
+            self.logger.log_stat("grad_norm", grad_norm.item(), t_env)
             mask_elems = mask_expanded.sum().item()
             self.logger.log_stat("td_error_abs", (masked_td_error.abs().sum().item() / mask_elems), t_env)
             self.logger.log_stat("q_taken_mean", (chosen_action_qvals * mask_expanded).sum().item() / (mask_elems * self.args.n_agents), t_env)
